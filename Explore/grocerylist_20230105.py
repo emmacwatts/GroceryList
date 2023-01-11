64851fbba1ad228fa3dc0c11bc8d0b6c48838212
emmawatts9 <115486994+emmawatts9@users.noreply.github.com>
2023-01-11 13:44:55 +0000

    add functions

--- Explore/grocerylist_20230105.py
+++ Explore/grocerylist_20230105.py
@@ -1,5 +1,6 @@
 #python code goes here
 import pandas as pd
+import random
 
 #Import files
 recipeIngredientsdf = pd.read_excel('~/GitHubProjects/GroceryList/InputFiles/RecipeIngredients.xlsx')
@@ -12,13 +13,29 @@ numberOfDinners = 7
 
 # print(ingredientTags)
 
+#Utils
 #Take in user recipe specs
 def userGrocerySpecs():
     dinnerVol, dinnersType = input("Enter the number of dinner recipes/portions desired. (e.g. 1 recipe or 5 portions) ").split()
     return dinnerVol, dinnersType
 
-dinnerVol, dinnersType = userGrocerySpecs()
-
 #Master df
 
 #Define recipes
+
+#Pick recipes
+def defineLunchesandDinners():
+    lunches = []
+    dinners = []
+    for recipe, type in zip(recipeInfodf["Recipe"], recipeInfodf["Meal Type"]):
+        if "Lunch" in type:
+            lunches.append(recipe)
+        if "Dinner" in type:
+            dinners.append(recipe)
+    return lunches, dinners
+
+def pickMeals():
+    
+#run.py
+dinnerVol, dinnersType = userGrocerySpecs()
+lunches, dinners = defineLunchesandDinners()
