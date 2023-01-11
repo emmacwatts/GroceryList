c82e860b966b39ede958a4db8bcec0546a6f30a2
emmawatts9 <115486994+emmawatts9@users.noreply.github.com>
2023-01-11 11:44:30 +0000

    clean-up

--- Explore/grocerylist_20230105.py
+++ Explore/grocerylist_20230105.py
@@ -1,20 +1,22 @@
 #python code goes here
 import pandas as pd
 
+data = pd.Series([1,2,3])
+print(data)
 #Import files
-recipeIngredientsdf = pd.read_excel('/Users/ecwatts/Desktop/Uni/Oxford DTP/GitHub Projects/GroceryList/InputFiles/RecipeIngredients.xlsx')
-recipeInfodf = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/RecipeInfo.xlsx')
-ingredientTags = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/IngredientTags.xlsx')
+# recipeIngredientsdf = pd.read_excel('../GroceryList/InputFiles/RecipeIngredients.xlsx')
+# recipeInfodf = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/RecipeInfo.xlsx')
+# ingredientTags = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/IngredientTags.xlsx')
 
 #Other parameters
 numberOfLunches = 7
 numberOfDinners = 7
 
-print(ingredientTags)
+# print(ingredientTags)
 
 #Take in user recipe specs
-def userGrocerySpecs():
-    dinnerVol, dinnersType = input("Enter the number of dinner recipes/portions desired. (e.g. 1 recipe or 5 portions)")
+#  def userGrocerySpecs():
+    # dinnerVol, dinnersType = input("Enter the number of dinner recipes/portions desired. (e.g. 1 recipe or 5 portions)")
 
 #Master df
 
--- Planning/NotestoSelf.md
+++ Planning/NotestoSelf.md
@@ -1,3 +1,5 @@
 ## Notes to self
 
 * To view pandas dfs, create a breakpoint after the df variable is made, run with debugger and then right-click the variable for df under locals, and click 'View Value in Data Viewer'
+
+* To comment out multiple lines of code on a mac - command + K + C. To uncomment - command + K + U
