05bf83d886bf04d8151ea2a42a4d9911b0a46dfd
emmawatts9 <115486994+emmawatts9@users.noreply.github.com>
2023-01-11 13:34:16 +0000

    pandas now works

--- Explore/grocerylist_20230105.py
+++ Explore/grocerylist_20230105.py
@@ -1,12 +1,10 @@
 #python code goes here
 import pandas as pd
 
-data = pd.Series([1,2,3])
-print(data)
 #Import files
-# recipeIngredientsdf = pd.read_excel('../GroceryList/InputFiles/RecipeIngredients.xlsx')
-# recipeInfodf = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/RecipeInfo.xlsx')
-# ingredientTags = pd.read_excel('/home/user/Documents/GitHub/GroceryList/GroceryList/InputFiles/IngredientTags.xlsx')
+recipeIngredientsdf = pd.read_excel('~/GitHubProjects/GroceryList/InputFiles/RecipeIngredients.xlsx')
+recipeInfodf = pd.read_excel('~/GitHubProjects/GroceryList/InputFiles/RecipeInfo.xlsx')
+ingredientTags = pd.read_excel('~/GitHubProjects/GroceryList/InputFiles/IngredientTags.xlsx')
 
 #Other parameters
 numberOfLunches = 7
@@ -15,8 +13,11 @@ numberOfDinners = 7
 # print(ingredientTags)
 
 #Take in user recipe specs
-#  def userGrocerySpecs():
-    # dinnerVol, dinnersType = input("Enter the number of dinner recipes/portions desired. (e.g. 1 recipe or 5 portions)")
+def userGrocerySpecs():
+    dinnerVol, dinnersType = input("Enter the number of dinner recipes/portions desired. (e.g. 1 recipe or 5 portions) ").split()
+    return dinnerVol, dinnersType
+
+dinnerVol, dinnersType = userGrocerySpecs()
 
 #Master df
 
--- Planning/NotestoSelf.md
+++ Planning/NotestoSelf.md
@@ -3,3 +3,5 @@
 * To view pandas dfs, create a breakpoint after the df variable is made, run with debugger and then right-click the variable for df under locals, and click 'View Value in Data Viewer'
 
 * To comment out multiple lines of code on a mac - command + K + C. To uncomment - command + K + U
+
+* Command that worked for python pip install was: (.venv) (base) Emmas-MacBook-Air-2:GroceryList ecwatts$ python -m pip install openpxyl
